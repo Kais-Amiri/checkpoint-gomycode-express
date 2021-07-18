a4e78fd476123ffa8a7f6e05f6cc0e9c93c60fd9
kais <ing.amiri.kais@gmail.com>
2021-07-18 12:13:15 +0100

    last version

--- server.js
+++ server.js
@@ -36,12 +36,3 @@ app.listen(8080, (err) => {
     console.log("server running on port 8080");
   }
 });
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "src", "webPages", "index.html"));
-// });
-// app.get("/services", (req, res) => {
-//   res.sendFile(path.join(__dirname, "src", "webPages", "services.html"));
-// });
-// app.get("/contact", (req, res) => {
-//   res.sendFile(path.join(__dirname, "src", "webPages", "contact.html"));
-// });
